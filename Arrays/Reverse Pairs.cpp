3f6b132f7293413607bcad88b79cf1cc8dc3d656
Bhulan Deb <94342258+bhulan@users.noreply.github.com>
2022-12-18 22:04:22 +0530

    Create Reverse Pairs.cpp

---
+++ Arrays/Reverse Pairs.cpp
@@ -0,0 +1,15 @@
+----------------//////////// ---------32 / 140 testcases passed--------------------//////////////----------
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]/2.0>nums[j])
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
