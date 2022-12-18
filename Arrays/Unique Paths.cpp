adf89f881045fd61ec86a4204d7b01d6199d473d
Bhulan Deb <94342258+bhulan@users.noreply.github.com>
2022-12-18 21:41:55 +0530

    Create Unique Paths.cpp

---
+++ Arrays/Unique Paths.cpp
@@ -0,0 +1,16 @@
+
+
+------------//Partially Accepted 37/63-----------//------
+class Solution {
+public:
+int solve(int i,int j,int m,int n){
+    if(i>m-1 || j>n-1)
+    return 0;
+    if(i==m-1 && j==n-1)
+    return 1;
+    return solve(i+1,j,m,n)+solve(i,j+1,m,n);
+}
+    int uniquePaths(int m, int n) {
+       return solve(0,0,m,n);
+    }
+};
