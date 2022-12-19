9981c6a9d279f676ffd4a2c5c21bbfc920b586b3
Bhulan Deb <94342258+bhulan@users.noreply.github.com>
2022-12-19 10:04:38 +0530

    Update 4Sum.cpp

--- Arrays/4Sum.cpp
+++ Arrays/4Sum.cpp
@@ -46,3 +46,49 @@ public:
         return ans;
     }
 };
+
+
+
+-----------------------------OR---------
+    
+    class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        for(unsigned int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+             for(unsigned int j=i+1;j<nums.size();j++){
+            if(j>i+1 && nums[j]==nums[j-1])
+            continue;
+            unsigned int l=j+1;
+            unsigned int r=nums.size()-1;
+            unsigned int sumrem=target-(nums[i]+nums[j]);
+             while(l<r){
+                 unsigned int sum=nums[l]+nums[r];
+                 if(sum<sumrem)
+                 {
+                     l++;
+                   
+                 }
+                 else if(sum>sumrem){
+                      r--;  
+                 }
+                 else{
+                    ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                      while(l<r && nums[l]==nums[l+1])
+                     l++;
+                      while(l<r && nums[r]==nums[r-1])
+                     r--;
+                     r--;
+                     l++;
+                 }
+                 
+             }
+            
+        }
+        }
+        return ans;
+    }
+};
