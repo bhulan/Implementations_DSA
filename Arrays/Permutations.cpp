07ccd3bf2441fddac61c3308c71c67a7cd3ab92b
Bhulan Deb <94342258+bhulan@users.noreply.github.com>
2023-01-26 08:51:56 +0530

    Create Permutations.cpp

---
+++ Arrays/Permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+void solve(vector<int>& nums,vector<vector<int>>&ans,int ind){
+    if(ind>=nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=ind;i<nums.size();i++){
+        swap(nums[i],nums[ind]);
+        solve(nums,ans,ind+1);
+        swap(nums[i],nums[ind]);
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+       int ind=0;
+        solve(nums,ans,ind);
+        return ans;
+    }
+};
